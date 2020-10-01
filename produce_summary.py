8315ad8071ccd93b2c0cfb81e789c21e36afa893
sunniraleigh <sraleigh@smith.edu>
2020-10-01 16:56:49 +0000

    deleted initial messy code

--- produce_summary.py
+++ produce_summary.py
@@ -1,47 +1,5 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
 
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print(f"Delivered {count} {melon}s for total of ${amount}")
-the_file.close()
-
-
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = words[1]
-    amount = words[2]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
-
-# Define a print deliveries function that consolidates the code above
+# Define a print deliveries function
 def print_deliveries(day_num, file_path):
     print(f"Day {day_num}")
     the_file = open(file_path)
