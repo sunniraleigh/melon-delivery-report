46cf9b9fc034f2f169354a0dc52ea21a3a7ae645
sunniraleigh <sraleigh@smith.edu>
2020-10-01 16:48:28 +0000

    define print deliveries function

--- produce_summary.py
+++ produce_summary.py
@@ -8,8 +8,7 @@ for line in the_file:
     count = words[0]
     amount = words[0]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+    print(f."Delivered {count} {melon}s for total of ${amount}")
 the_file.close()
 
 
@@ -41,3 +40,20 @@ for line in the_file:
     print("Delivered {} {}s for total of ${}".format(
         count, melon, amount))
 the_file.close()
+
+# Define a print deliveries function that consolidates the code above
+def print_deliveries(day_num, file_path):
+    print(f."Day {day_num}")
+    the_file = open(file_path)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[0]
+        amount = words[0]
+
+        print("Delivered {} {}s for total of ${}".format(
+            count, melon, amount))
+    the_file.close()
+
